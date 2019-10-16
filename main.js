4b5adcf71aff88d29da3ff73fb4428753d123f11
Andreina-Romero <andreina.romero.garcia@gmail.com>
2019-10-16 23:05:49 +0200

    Add functions on mobile and desktop in order to take acelerometer orientation or mouse position

--- main.js
+++ main.js
@@ -5,10 +5,19 @@ const backgrounds = document.querySelectorAll(".project__bg");
 const range = 40;
 
 const calcValue = (a, b) => (((a * 100) / b) * (range / 100) -(range / 2));
-// const calcValue = (a, b) => (a/b*range-range/2).toFixed(1)
 
-let timeout;
-document.addEventListener('mousemove', ({x, y}) => {
+function isDeviceMobile(){
+  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  return isMobile;
+}
+
+function hasDeviceOrientation(){
+  const hasOrientation = window.DeviceOrientationEvent? true : false;
+  return hasOrientation;
+}
+
+const moveOnDesktop = ({x,y}) => {
+  let timeout;
   if (timeout) {
     window.cancelAnimationFrame(timeout);
   }
@@ -27,24 +36,55 @@ document.addEventListener('mousemove', ({x, y}) => {
       background.style.backgroundPosition = `${xValue*.45}px ${-yValue*.45}px`;
     })
 	})
-}, false);
-
-
-//DEVICE*******************⭐️
-
-if(window.DeviceOrientationEvent) {
-  console.log("Browser supports the device orientation");
-  //registering deviceorientation event listener
-  window.addEventListener("deviceorientation", process, false);
-} else {
-  // The browser does not support the device orientation event
-   console.log("Browser does not support the device orientation");
 }
 
-function process(event) {
-//put the code to be executed when  device orientation is changed
-console.log("Device Orientation changed");
-var alpha = event.alpha;
-var beta = event.beta;
-var gamma = event.gamma;
-console.log("Alpha : " + alpha + "Beta : " + beta + "Gamma : " + gamma ); 
+function moveOnRotateMobile(event) {
++
+  console.log("Device Orientation changed");
+  var beta = event.beta;
+  var gamma = event.gamma;
+
+  let timeout;
+  if (timeout) {
+    window.cancelAnimationFrame(timeout);
+  }
+  	
+  timeout = window.requestAnimationFrame(() => {
+   if(gamma >= 10){
+     gamma = 10;
+   }
+   else if(gamma < -10){
+     gamma = -10;
+   }
+  
+   projects.style.transform = `rotateY(${gamma}deg) rotateX(${beta/15}deg)`;
+
+    [].forEach.call(images, (image) => {
+      var nextVal = (- (gamma/2)*2).toFixed();
+      var anotherVal = ( (-gamma*2)).toFixed();
+      image.style.transform = `translateX(${-nextVal}px) translateY(${anotherVal}px)`;
+      image.style.transform = `translateX(${-((gamma)*1).toFixed()}px) translateY(${(beta-45).toFixed()}px)`;
+    });
+
+    [].forEach.call(backgrounds, (background) => {
+      background.style.backgroundPosition = `${(gamma*.5).toFixed()}px ${(-beta*.5).toFixed()}px`;
+    })
+	})
+} 
+
+
+const isMobile = isDeviceMobile();
+const hasOrientation = hasDeviceOrientation();
+
+
+if(isMobile && hasOrientation){
+  console.log("Yes, has orientation and is a MOBILE");
+  window.addEventListener("deviceorientation", moveOnRotateMobile);
+}
+else if(!isMobile && hasOrientation){
+  console.log("Yes, has orientation BUT is a COMPUTER");
+  document.addEventListener('mousemove', moveOnDesktop);
+}
+else {
+  console.log("Your browser does not support the animation :(")
+}
